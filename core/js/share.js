f068a6dcae25bfc44f745a8c0c57ff39465f7d87
Joas Schilling <nickvergessen@gmx.de>
2014-03-26 15:20:39 +0100

    Add a fallback for old share() calls without expirationDate. We should remove this in a later version, after the Apps have been updated.
    Fix #4823

--- core/js/share.js
+++ core/js/share.js
@@ -137,6 +137,17 @@ OC.Share={
 		return data;
 	},
 	share:function(itemType, itemSource, shareType, shareWith, permissions, itemSourceName, expirationDate, callback) {
+		// Add a fallback for old share() calls without expirationDate.
+		// We should remove this in a later version,
+		// after the Apps have been updated.
+		if (typeof callback === 'undefined' && typeof expirationDate === 'function') {
+			callback = expirationDate;
+			expirationDate = '';
+			console.warn("Call to 'OC.Share.share()' with too few arguments. "
+				+ "'expirationDate' was assumed to be 'callback'. "
+				+ "Please revisit the call and fix the list of arguments.");
+		}
+
 		$.post(OC.filePath('core', 'ajax', 'share.php'),
 			{
 				action: 'share',
